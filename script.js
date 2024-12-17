c4a3b56ba3868a700e160c24c49ee4d6005d13a3
nicolo_canevari <nico.canevari@outlook.it>
2024-12-17 14:58:29 +0100

    Logica esercizio parte 2

--- script.js
+++ script.js
@@ -12,11 +12,22 @@
 
 // INPUT:
 //  Il programma dovrà chiedere all'utente il numero dei chilometri che vuole percorrere.
+let userKm = prompt ("indica il numero di km che vuoi percorrere")
+
 // Il programma dovrà chiedere all'utente l'età del passeggero.
+let userAge = prompt ("indica l'età del passeggero")
+
+console.log(userKm, userAge);
+
 
 // ELABORAZIONE:
 
 // Sulla base dei dati raccolti nell'INPUT il programma dovrà calcolare il prezzo totale del viaggio (il prezzo del biglietto è di 0,21€ al km).
+function calcolaPrezzo(userKm, userAge) {
+    // Prezzo per chilometro
+    const prezzoPerKm = 0.21;
+}
+
    // Se il passeggero ha meno di 18 anni avrà uno sconto del 20% sul costo totale del biglietto.
    // Se il passeggero ha più di 65 anni avrà uno sconto del 40% sul costo totale del biglietto.
    // Se il passeggero ha un età compresa tra 18 e 65 anni non avrà nessuno sconto sul costo totale del biglietto.
